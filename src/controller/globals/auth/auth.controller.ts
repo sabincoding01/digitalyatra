d27483aa766477bfeefe587da6fe9b3cea9b927b
sabin <sabintimalsina74@gmail.com>
2025-11-26 22:13:46 +0545

    encryption, decriptio, and hashing

---
+++ src/controller/globals/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+exports.registerUser = () =>{
+
+}
+
+exports.loginUser = () =>{
+
+}
+
+class AuthController{
+static async registeruser(){
+
+}
+
+static async loginUser(){
+
+}
+
+}
+module.exports = AuthController;
